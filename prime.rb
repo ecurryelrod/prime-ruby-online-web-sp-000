1ecf0edd05f53b6395db89e1a1cbc0a438e5ca72
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-04-03 19:21:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,7 @@
 # Add  code here!
 def prime?(integer)
   (2..(integer - 1)).each do |num| 
-    return false if integer % num == 0
-    return false if integer < 2
+    return false if integer % num == 0 || return false if integer < 2
   end
   true
 end
